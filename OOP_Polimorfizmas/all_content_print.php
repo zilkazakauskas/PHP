a7040e45c33bf9e606a15f4afd2be098173b1427
Zilvinas <zil.kazakauskas@gmail.com>
2021-02-02 20:10:54 +0200

    kodo pakeitimai

--- OOP_Polimorfizmas/all_content_print.php
+++ OOP_Polimorfizmas/all_content_print.php
@@ -11,25 +11,22 @@ while ($row = mysqli_fetch_array($result)) {
 }
 $publification->printAllArticleContent();
 
-$sql = "SELECT temos.pavadinimas,articles.id FROM articles,straipsniai_temos,temos WHERE straipsniai_temos.straipsnio_id=articles.id AND straipsniai_temos.temos_id=temos.id AND articles.id=" . $_GET['id'] . ";";
+//ISPRINTINA TEMAS
+
+$sql = "SELECT temos.pavadinimas,articles.id FROM articles,straipsniai_temos,temos 
+        WHERE straipsniai_temos.straipsnio_id=articles.id AND straipsniai_temos.temos_id=temos.id AND articles.id=" . $_GET['id'] . ";";
 $result = mysqli_query($link, $sql);
 
-if(!mysqli_connect_errno()){
-echo '<h2>Temos:</h2>';
+if (!mysqli_connect_errno()) {
+    echo '<h2>Temos:</h2>';
+    while ($row = mysqli_fetch_array($result)) {
+        echo $row['pavadinimas'] . "<br>";
+    }
 }
 
-while ($row = mysqli_fetch_array($result)) {
-    echo $row['pavadinimas']."<br>";
-}
+
 
 //PRINTINA NUOTRAUKAS IS PREVIEW_IMAGES
 
-$sgl = "SELECT image FROM preview_images WHERE article_id = " . $_GET['id'] . ";";
-$result = mysqli_query($link, $sgl);
-
-while ($row = mysqli_fetch_array($result)) {
-    ?>
-    <img src="<?php echo $row['image'] ?>" alt="image">
-    <?php
-}
+require 'preview_photos.php';
 ?>
